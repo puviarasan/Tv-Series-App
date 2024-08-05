5d228387e755c5ad880653e41aa6296f342c672c
sppuviarasan <puviek7@gmail.com>
2024-08-05 20:41:34 +0530

    Initial commit

---
+++ app/src/main/java/com/ek/tvseries/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ek.tvseries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ek.tvseries.ui.Screen
+import com.ek.tvseries.ui.theme.TvSeriesTheme
+import com.ek.tvseries.ui.tv_series_detail.components.TvSeriesDetailComposable
+import com.ek.tvseries.ui.tv_series_list.components.TvSeriesComposable
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TvSeriesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
